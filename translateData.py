5ae79a41c578b13966f4d6ff787c89f60d7fb378
KylevdLangemheen <kylevdlangemheen@live.nl>
2018-10-20 09:54:34 +0200

    Kmeans impossible on the data. fixed translation. working on kmedoids

--- translateData.py
+++ translateData.py
@@ -1,4 +1,4 @@
-import os
+#translate the supplied data format into preferred format
 sourceName = "HA_complete_all.fasta"        #source file to read
 goalName = "HA_t_complete_all.fasta"        #goal file to write
 
@@ -9,9 +9,11 @@ nextLabel = True
 FASTA = ""
 for line in sourceFile:
     if nextLabel:
-        goalFile.write(FASTA)
-        FASTA = ""
         goalFile.write(line)
         nextLabel = False
     else:
         FASTA = FASTA + line.rstrip("\n")
+    if line == "\n":
+        goalFile.write(FASTA + "\n")
+        FASTA = ""
+        nextLabel = True
